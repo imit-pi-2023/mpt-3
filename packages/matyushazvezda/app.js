f1e0665e917611152a3219fd634766091e34bd0b
matyushazvezda <matyushazvezda@yandex.ru>
2023-09-27 01:19:33 +0800

    added Safronov Matvey

---
+++ packages/matyushazvezda/app.js
@@ -0,0 +1,13 @@
+function calculateDaysUntilNewYear(currentDate = new Date()) {
+    const newYearDate = new Date(currentDate.getFullYear(), 11, 31);
+    const timeDiff = newYearDate.getTime() - currentDate.getTime();
+    const daysRemaining = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+    return daysRemaining;
+  }
+  
+  console.log(`До Нового года осталось ${calculateDaysUntilNewYear()} дней.`);
+  
+  module.exports = {
+    calculateDaysUntilNewYear,
+  };
+  
