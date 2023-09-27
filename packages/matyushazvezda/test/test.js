367c5ede2c88f841fbf03645c75334f83c400c08
matyushazvezda <matyushazvezda@yandex.ru>
2023-09-27 11:55:39 +0800

    new prettier add

--- packages/matyushazvezda/test/test.js
+++ packages/matyushazvezda/test/test.js
@@ -1,16 +1,16 @@
-const assert = require('chai').assert;
-const { calculateDaysUntilNewYear } = require('../app'); 
+const assert = require("chai").assert;
+const { calculateDaysUntilNewYear } = require("../app");
 
-describe('calculateDaysUntilNewYear', function () {
-    it('должна возвращать положительное число дней до Нового года', function () {
-        const result = calculateDaysUntilNewYear();
-        assert.isNumber(result);
-        assert.isAbove(result, 0);
-      });
+describe("calculateDaysUntilNewYear", function () {
+  it("должна возвращать положительное число дней до Нового года", function () {
+    const result = calculateDaysUntilNewYear();
+    assert.isNumber(result);
+    assert.isAbove(result, 0);
+  });
 
-      it('должна возвращать 0, если сегодня уже Новый год', function () {
-        const currentDate = new Date('2023-12-31'); 
-        const result = calculateDaysUntilNewYear(currentDate);
-        assert.equal(result, 0);
-      });
+  it("должна возвращать 0, если сегодня уже Новый год", function () {
+    const currentDate = new Date("2023-12-31");
+    const result = calculateDaysUntilNewYear(currentDate);
+    assert.equal(result, 0);
+  });
 });
