ff785dbf6df48e9354272a88854025c2ca7a2ca6
aplokion <aplok10n@mail.ru>
2023-09-27 10:35:34 +0800

    make it prettier

--- packages/aplokion/methode.ts
+++ packages/aplokion/methode.ts
@@ -1,6 +1,10 @@
-export function new_year(){
-    let current_date = new Date();  
-    let new_year = new Date(current_date.getFullYear()+1, 0, 1)
+export function new_year() {
+  let current_date = new Date();
+  let new_year = new Date(current_date.getFullYear() + 1, 0, 1);
 
-    console.log(`${Math.ceil((Number(new_year) - Number(current_date)) / (1000 * 60 * 60 * 24))} дней`) 
+  console.log(
+    `${Math.ceil(
+      (Number(new_year) - Number(current_date)) / (1000 * 60 * 60 * 24)
+    )} дней`
+  );
 }
