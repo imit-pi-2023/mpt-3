264765c930551213b124b553a39f4b1f4eaf6371
Nikita Popov <nikitapopov2002@yandex.ru>
2023-09-28 03:43:44 +0800

    94

--- packages/nikpopo/test/module.spec.js
+++ packages/nikpopo/test/module.spec.js
@@ -2,7 +2,7 @@ import { daysUntilNewYear } from "../method.js";
 import { expect } from "chai";
 
 describe("Example testing of module `method`", () => {
-    it("new_year(new Date(2024,0,1)) should be equal to 95", () => {
-        expect(daysUntilNewYear(new Date())).to.be.equal(95);
+    it("new_year(new Date(2024,0,1)) should be equal to 94", () => {
+        expect(daysUntilNewYear(new Date())).to.be.equal(94);
     });
 });
