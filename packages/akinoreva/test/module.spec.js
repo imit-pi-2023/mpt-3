bda6a884c8574b777f54dad612ad59b6bf92919e
akinoreva <144860708+akinoreva@users.noreply.github.com>
2023-09-27 10:32:06 +0800

    Add files via upload

---
+++ packages/akinoreva/test/module.spec.js
@@ -0,0 +1,8 @@
+import { daysBeforeNewYear, date23 } from "../method.js";
+import { expect } from "chai";
+
+describe("Example testing of module `method`", () => {
+  it("date23 should be equal to 365", () => {
+    expect(daysBeforeNewYear(date23)).to.be.equal(365);
+  });
+});
