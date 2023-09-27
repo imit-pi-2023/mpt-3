505eb8b8902cec33e72642691fc535343eb08cdf
Igor Smirnov <moguntruba@gmail.com>
2023-09-27 10:25:51 +0800

    3

--- packages/ledertoo/test/module.spec.js
+++ packages/ledertoo/test/module.spec.js
@@ -4,13 +4,12 @@ import { expect } from "chai";
 
 describe("Example testing of module `module`", () => {
   it("should export day 2023.01.01 equal to 364", () => {
-    expect(last_time (dayz)).to.be.equal(364);
+    expect(last_time(dayz)).to.be.equal(364);
   });
 
   describe("function last_time", () => {
     it("should return 95, when  last_time(today) called", () => {
       expect(last_time(today)).to.be.equal(95);
     });
-
   });
 });
