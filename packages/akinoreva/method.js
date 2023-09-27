ba65467346d3d02c11667c9bc5bf9d943ff6d703
akinoreva <144860708+akinoreva@users.noreply.github.com>
2023-09-27 10:01:25 +0800

    Update method.js

--- packages/akinoreva/method.js
+++ packages/akinoreva/method.js
@@ -1,9 +1,8 @@
 export const date23 = new Date("2023-01-01T00:00:00");
 
-export function daysBeforeNewYear(today){
+export function daysBeforeNewYear(today) {
+  let year = today.getFullYear() + 1;
+  let newYear = new Date(year, 0, 1, 0, 0, 0, 0);
 
-    let year = today.getFullYear()+1;
-    let newYear = new Date(year,0,1,0,0,0,0);
-    
-    return Math.ceil((newYear-today)/86400000);
+  return Math.ceil((newYear - today) / 86400000);
 }
